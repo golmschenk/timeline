f2c45f69254c2cef9e2afde35451b6fc2589a7da
Greg Olmschenk <golmschenk@gmail.com>
2014-09-25 16:35:56 -0400

    Add jinja2 and basic template

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import jinja2
+import os
 import webapp2
 from timeline import Timeline
 
-timeline = Timeline()
-timeline.get_data_from_local_csv()
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader([os.path.dirname(__file__), os.path.dirname(__file__) + "/templates"]))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template('index.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)

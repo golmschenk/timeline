df8836cf9e28611cadda02447208ab37cfb8e72c
Greg Olmschenk <golmschenk@gmail.com>
2014-09-25 16:24:17 -0400

    Output from simple local csv

--- timeline.py
+++ timeline.py
@@ -1,6 +1,14 @@
+import csv
+import urllib2
+import secret
+
 class Timeline():
     def __init__(self):
         self.category_dict = {}
 
-    def get_data_from_spreadsheet_csv(self):
-        pass
+    def get_data_from_local_csv(self):
+        with open("data.csv", 'r') as csv_file:
+            csv_content = csv.reader(csv_file)
+            for row in csv_content:
+                for col in row:
+                    print col
